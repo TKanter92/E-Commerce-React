1e6c6df7935466424eb7a38f9a6cf5a89f6ecb4a
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-09-30 11:26:32 -0400

    Created NavBar component and added in a class component under App.jsx file

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const NavigationBar = ({ user }) => {
+    return(
+        <React.Fragment>
+            <div>
+                <Link to= '/'>Home</Link>
+            </div>
+            <div>
+                <Link to= '/login'>Login</Link>
+            </div>
+            <div>
+                <Link to= '/shoppingcart'>Shopping Cart</Link>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default NavigationBar;
